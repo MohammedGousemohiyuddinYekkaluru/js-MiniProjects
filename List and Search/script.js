a935260b5a1395fedd2c78ab21a14908a544235e
mohammed gouse <gousemohammed163@gmail.com>
2025-07-24 10:43:48 +0530

    List and Search

---
+++ List and Search/script.js
@@ -0,0 +1,12 @@
+const array = [
+    {
+        profileUrl:"https://imgs.search.brave.com/VowRd5WcnObqa4fCTWIqYGT73zyzPGIsZSmkXFmo-sA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/bW9kZWwtaW4td2Fy/bS1saWdodC1wb3Np/bmcuanBnP3dpZHRo/PTEwMDAmZm9ybWF0/PXBqcGcmZXhpZj0w/JmlwdGM9MA",
+        name: "julie",
+        email:"julie@mail.com"
+    },
+    {
+       profileUrl:"https://imgs.search.brave.com/7Z5-AlaSi-2YWGhT1YahX-moQvQr7w40EYXrQA95Byg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzIzLzg2/LzIxLzIzODYyMTE2/ODAzMTc4MjRjYTE0/YzM4ZjZiOWJlM2Zj/LmpwZw",
+        name: "john",
+        email:"john@mail.com"  
+    }
+]
