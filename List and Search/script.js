5f417019e4854be5790a0da245e0af0a7e364d89
mohammed gouse <gousemohammed163@gmail.com>
2025-07-25 17:17:05 +0530

    List and Search

--- List and Search/script.js
+++ List and Search/script.js
@@ -42,7 +42,7 @@ renderUsers(users);
 function handleSearch (e) {
     let searchValue = e.target.value;
     let filteredUsers = users.filter((obj) => {
-        return obj.name.toLowerCase().includes(searchValue.toLowerCase());
+        return obj.name.toLowerCase().includes(searchValue.toLowerCase()) || obj.email.toLowerCase().includes(searchValue.toLowerCase())
     });
     renderUsers(filteredUsers);
 }
